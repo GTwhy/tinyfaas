78eb5659adc39a53098b61608b0cf930a9a34f74
why <wang.hengyu@foxmail.com>
2021-03-17 22:09:47 +0800

    Add multithreading support to worker_test.
    I did some tests with this test program and found that :
    1. Sometimes it will crash with segmentation fault.
    2. Sometimes single thread is enough for light and nonblocking workload. Multithreading is more suitable for heavy and blocking workload which you can use sleep() in the workload program to emulate.

--- core/worker/worker_test.c
+++ core/worker/worker_test.c
@@ -10,8 +10,13 @@
 #include <nng/protocol/reqrep0/req.h>
 #include <nng/supplemental/util/platform.h>
 #include "work_server.h"
+#include <pthread.h>
+#include <sys/wait.h>
+#define THREAD_NUMBER 2
+#define COUNT 10000
+#define DEBUG
 
-#define COUNT 1000
+
 //TODO:消息结构体，以及序列化和反序列化问题需要进一步考虑。
 struct func_msg_trans{
 	uint32_t app_id;
@@ -25,6 +30,15 @@ struct args{
 	int m;
 	int n;
 };
+
+nng_time   start;
+nng_time   end;
+
+static int count;
+
+static int send_count = 0;
+
+pthread_mutex_t th_mutex;
 
 void
 fatal(const char *func, int rv)
@@ -40,8 +54,6 @@ func_client(const char *func_url, const char *work_url)
 	nng_socket sock;
 	int        rv;
 	nng_msg *  msg;
-	nng_time   start;
-	nng_time   end;
 	struct func_msg_trans funcMsg;
 
 	funcMsg.app_id = 0;
@@ -49,11 +61,11 @@ func_client(const char *func_url, const char *work_url)
 	strcpy(funcMsg.url, work_url);
 	strcpy(funcMsg.func_path, "./libwork.so");
 	strcpy(funcMsg.func_name, "work");
-	 
+
 	if ((rv = nng_req0_open(&sock)) != 0) {
 		fatal("nng_req0_open", rv);
 	}
-	 
+
 	if ((rv = nng_dial(sock, func_url, NULL, 0)) != 0) {
 		fatal("nng_dial", rv);
 	}
@@ -63,19 +75,19 @@ func_client(const char *func_url, const char *work_url)
 	if ((rv = nng_msg_alloc(&msg, 0)) != 0) {
 		fatal("nng_msg_alloc", rv);
 	}
-	 
+
 	if ((rv = nng_msg_append(msg, &funcMsg, sizeof(funcMsg))) != 0) {
 		fatal("nng_msg_append_u32", rv);
 	}
-	 
+
 	if ((rv = nng_sendmsg(sock, msg, 0)) != 0) {
 		fatal("nng_send", rv);
 	}
-	 
+
 	if ((rv = nng_recvmsg(sock, &msg, 0)) != 0) {
 		fatal("nng_recvmsg", rv);
 	}
-	 
+
 	end = nng_clock();
 	nng_msg_free(msg);
 	nng_close(sock);
@@ -84,15 +96,20 @@ func_client(const char *func_url, const char *work_url)
 	return (0);
 }
 
-int
-work_client(const char *work_url)
+void end_print()
 {
+	end = nng_clock();
+	printf("Func request took %u milliseconds.\n", (uint32_t)(end - start));
+	exit(0);
+}
+
+void *
+work_client(void * url)
+{
+	const char * work_url = url;
 	nng_socket sock;
 	int        rv;
 	nng_msg *  msg;
-
-	nng_time   start;
-	nng_time   end;
 	struct args arg;
 	arg.m = 100;
 	arg.n = 999;
@@ -107,7 +124,7 @@ work_client(const char *work_url)
 	if ((rv = nng_msg_alloc(&msg, 0)) != 0) {
 		fatal("nng_msg_alloc", rv);
 	}
-	start = nng_clock();
+	struct nng_msg * recv_msg;
 	for(int i = 0; i < COUNT; i++){
 		if ((rv = nng_msg_append(msg, &arg, sizeof(arg))) != 0) {
 			fatal("nng_msg_append_u32", rv);
@@ -116,27 +133,33 @@ work_client(const char *work_url)
 		if ((rv = nng_sendmsg(sock, msg, 0)) != 0) {
 			fatal("nng_send", rv);
 		}
-		struct nng_msg * recv_msg;
+		pthread_mutex_lock(&th_mutex);
+//#ifdef DEBUG
+//		printf("send count : %d\n", ++send_count);
+//#endif
+		pthread_mutex_unlock(&th_mutex);
 		nng_msg_alloc(&recv_msg, 0);
 		if ((rv = nng_recvmsg(sock, &recv_msg, 0)) != 0) {
 			fatal("nng_recvmsg", rv);
 		}
 		mate_date_s * mp = (mate_date_s *)nng_msg_body(recv_msg);
 		printf("Content of recv_msg : %u  %u\n", mp->data_id, mp->data_stat);
-		nng_msg_free(recv_msg);
+		pthread_mutex_lock(&th_mutex);
+#ifdef DEBUG
+		printf("count : %d\n", count);
+#endif
+		if(++count >= COUNT)
+			end_print();
+		pthread_mutex_unlock(&th_mutex);
 	}
-	end = nng_clock();
-	nng_msg_free(msg);
-	nng_close(sock);
-
-	printf("Work request took %u milliseconds.\n", (uint32_t)(end - start));
 	return (0);
 }
+
+
 
 int
 main(int argc, char **argv)
 {
-	int rc;
 
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s <func_url> <work_url>\n", argv[0]);
@@ -144,6 +167,15 @@ main(int argc, char **argv)
 	}
 	func_client(argv[1], argv[2]);
 	sleep(3);
-	rc = work_client(argv[2]);
-	exit(rc == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+	count = 0;
+	pthread_mutex_init(&th_mutex, NULL);
+	pthread_t pts[THREAD_NUMBER];
+
+	for (int i = 0; i < THREAD_NUMBER; ++i) {
+		pthread_create(&pts[i], NULL, work_client, argv[2]);
+	}
+	start = nng_clock();
+	for (;;) {
+		nng_msleep(3600000); // neither pause() nor sleep() portable
+	}
 }
