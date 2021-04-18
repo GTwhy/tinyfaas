b82f76803e855a72d74b22b2cb5519ca32cb7c4d
why <wang.hengyu@foxmail.com>
2021-04-18 20:08:30 +0800

    Upgrade auto-build.sh.

--- auto-build.sh
+++ auto-build.sh
@@ -1,19 +1,32 @@
+#!/bin/bash
+# Check cmake version
+ver=`cmake --version | head -n 1 | awk -F " " '{print $3}'`
+ver1=`echo $ver | awk -F . '{print $1}'`
+ver2=`echo $ver | awk -F . '{print $2}'`
+echo $ver $ver1 $ver2
+if [ $ver1 -lt 4 ] && [ $ver2 -lt 13 ]
+then
+  echo "cmake version should be higher than 3.13"
+  exit
+fi
+
+# Check nng library
 if [ -e "/usr/local/include/nng" ]
 then
 	echo nng exists
 else
 
+# Get nng src
   if ! [ -e "./lib/nng/src" ]
   then
       echo Can not find nng src files.
       echo Git clone nng from Gitee.
-      echo Make sure the cmake --version is higher than 3.16
       mkdir lib &&
       cd ./lib &&
       git clone https://gitee.com/mirrors/nng.git &&
       cd ..
   fi
-
+# Build nng
 	echo Build nng first.
 	cd ./lib/nng/ &&
 	mkdir build
@@ -25,6 +38,7 @@ else
 
 fi
 
+# Build targets
 if ! [ -e "./build" ]
 then
   mkdir build
