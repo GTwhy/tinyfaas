b748e470650e4bca9f69f1a3eea19ce323360079
why <wang.hengyu@foxmail.com>
2021-04-22 03:16:16 +0800

    Add demo : concurrent.

---
+++ demo/concurrent/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+COUNT=1000
+
+cd ../../build
+./lutf &
+sleep 1
+echo start_lutf done
+cd ../demo/concurrent
+
+typeset -a CLIENT_PID
+i=0
+
+
+./add_func  &
+echo add_func done.
+sleep 1
+
+
+while (( i < COUNT ))
+do
+	i=$(( i + 1 ))
+	rand_time=$(( RANDOM % 10000 ))
+	echo "Starting client $i"
+	./client $rand_time &
+	eval CLIENT_PID[$i]=$!
+done
+
+i=0
+while (( i < COUNT ))
+do
+	let "i++"
+	wait ${CLIENT_PID[$i]}
+done
+
+echo stop lutf-servers.
+../../bin/stop_worker.sh
